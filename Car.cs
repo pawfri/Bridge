f6b4fee3e925a777bea7c10ac43b22ddcb68dea5
Patrick <pfriis@outlook.com>
2025-09-19 11:59:42 +0200

    Implemented Car class with properties, constructor and first-draft documentation

---
+++ Car.cs
@@ -0,0 +1,22 @@
+﻿namespace Bridge;
+
+/// <summary>
+/// Class representing a car with a license plate and date
+/// </summary>
+public class Car
+{
+    public string Licenseplate { get; set; }
+    public DateTime Date { get; set; }
+
+    /// <summary>
+    /// Initializes a new instace of the Car class with license plate and date
+    /// </summary>
+    /// <param name="licenseplate">A string consisting of numbers and letters</param>
+    /// <param name="date">Date associated with the car</param>
+    public Car(string licenseplate, DateTime date)
+    {
+        Licenseplate = licenseplate;
+        Date = date;
+    }
+
+}
