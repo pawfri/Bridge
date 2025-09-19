a255ac6a9b1887d6d8cfa47033f2c7f7f0a0643c
Patrick <pfriis@outlook.com>
2025-09-19 14:20:54 +0200

    Implemented MC class, Price and VehicleType methods, and documentation

---
+++ Bridge/MC.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge;
+
+/// <summary>
+/// Class representing a MC (Motorcycle) with a license plate and date
+/// </summary>
+public class MC
+{
+    public string Licenseplate { get; set; }
+    public DateTime Date { get; set; }
+
+
+    /// <summary>
+    /// Initializes a new instace of the MC class with license plate and date
+    /// </summary>
+    /// <param name="licenseplate">A string consisting of numbers and letters</param>
+    /// <param name="date">Date associated with the car</param>
+    public MC(string licenseplate, DateTime date)
+    { 
+        Licenseplate = licenseplate; 
+        Date = date; 
+    }
+
+    /// <summary>
+    /// Returns fixed price as a double
+    /// </summary>
+    /// <returns>The price of the vehicle</returns>
+    public double Price()
+    {
+        return 120.0;
+    }
+
+    /// <summary>
+    /// Returns vehicle type as a string
+    /// </summary>
+    /// <returns>The type of vehicle</returns>
+    public string VehicleType()
+    {
+        return "MC";
+    }
+}
--- BridgeTests/CarTests.cs
+++ BridgeTests/CarTests.cs
@@ -6,75 +6,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BridgeTests
+namespace BridgeTests;
+
+[TestClass()]
+public class CarTests
 {
-    [TestClass()]
-    public class CarTests
+
+    [DataRow(230.0)]
+    [DataRow(230)]
+    [TestMethod()]
+    public void PriceEqualTest(double expected)
     {
+        // Arrange
+        var car = new Car("AB35987", DateTime.Today);
 
-        [DataRow(230.0)]
-        [DataRow(230)]
-        [TestMethod()]
-        public void PriceEqualTest(double expected)
-        {
-            // Arrange
-            var car = new Car("AB35987", DateTime.Today);
+        // Act
+        double actual = car.Price();
 
-            // Act
-            double actual = car.Price();
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
 
-            // Assert
-            Assert.AreEqual(expected, actual);
-        }
+    [DataRow(229.0)]
+    [DataRow(229.9)]
+    [DataRow(230.1)]
+    [DataRow(231.0)]
+    [DataRow(231)]
+    [TestMethod()]
+    public void PriceNotEqualTest(double notExpected)
+    {
+        // Arrange
+        var car = new Car("AB35987", DateTime.Today);
 
-        [DataRow(229.0)]
-        [DataRow(229.9)]
-        [DataRow(230.1)]
-        [DataRow(231.0)]
-        [DataRow(231)]
-        [TestMethod()]
-        public void PriceNotEqualTest(double notExpected)
-        {
-            // Arrange
-            var car = new Car("AB35987", DateTime.Today);
+        // Act
+        double actual = car.Price();
 
-            // Act
-            double actual = car.Price();
+        // Assert
+        Assert.AreNotEqual(notExpected, actual);
+    }
 
-            // Assert
-            Assert.AreNotEqual(notExpected, actual);
-        }
+    [DataRow("Car")]
+    [TestMethod()]
+    public void VehicleTypeEqualTest(string exepected)
+    {
+        // Arrange
+        var car = new Car("AB35987", DateTime.Today);
 
-        [DataRow("Car")]
-        [TestMethod()]
-        public void VehicleTypeEqualTest(string exepected)
-        {
-            // Arrange
-            var car = new Car("AB35987", DateTime.Today);
+        // Act
+        string actual = car.VehicleType();
 
-            // Act
-            string actual = car.VehicleType();
+        // Assert
+        Assert.AreEqual(exepected, actual);
+    }
 
-            // Assert
-            Assert.AreEqual(exepected, actual);
-        }
+    [DataRow("bus")]
+    [DataRow("car")]
+    [DataRow("Cars")]
+    [DataRow("train")]
+    [DataRow("mc")]
+    [TestMethod()]
+    public void VehicleTypeNotEqualTest(string notExepcted)
+    {
+        // Arrange
+        var car = new Car("AB35987", DateTime.Today);
 
-        [DataRow("bus")]
-        [DataRow("car")]
-        [DataRow("Cars")]
-        [DataRow("train")]
-        [DataRow("mc")]
-        [TestMethod()]
-        public void VehicleTypeNotEqualTest(string notExepcted)
-        {
-            // Arrange
-            var car = new Car("AB35987", DateTime.Today);
+        // Act
+        string actual = car.VehicleType();
 
-            // Act
-            string actual = car.VehicleType();
-
-            // Assert
-            Assert.AreNotEqual(notExepcted, actual);
-        }
+        // Assert
+        Assert.AreNotEqual(notExepcted, actual);
     }
 }
