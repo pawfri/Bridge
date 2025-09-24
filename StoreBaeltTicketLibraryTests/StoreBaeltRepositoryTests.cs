30bf66258814b619d6dc6f40155e64c829494467
Patrick <pfriis@outlook.com>
2025-09-24 19:50:21 +0200

    Add unit test for all methods in StoreBaeltRepository

--- StoreBaeltTicketLibraryTests/StoreBaeltRepositoryTests.cs
+++ StoreBaeltTicketLibraryTests/StoreBaeltRepositoryTests.cs
@@ -33,4 +33,38 @@ public class StoreBaeltRepositoryTests
         // Assert
         Assert.AreEqual(licensePlate, StoreBaeltRepository.GetAll()[0].LicensePlate);
     }
+
+    [TestMethod]
+    public void GetAllTicketsTest()
+    {
+        // Arrange
+        var car = new Car("YX12389", false);
+        var car2 = new MC("BX87456", true);
+
+        // Act
+        StoreBaeltRepository.AddTicket(car);
+        StoreBaeltRepository.AddTicket(car2);
+
+        // Assert
+        Assert.AreEqual(2, StoreBaeltRepository.GetAll().Count);
+    }
+
+    [TestMethod]
+    public void GetTicketsLicensePlateTest()
+    {
+        // Arrange
+        var ticket = new Car("YX12389", false);
+        var ticket2 = new Car("YX12389", false);
+        var ticket3 = new Car("BX87456", true);
+        var ticket4 = new MC("CE87953", true);
+
+        // Act
+        StoreBaeltRepository.AddTicket(ticket);
+        StoreBaeltRepository.AddTicket(ticket2);
+        StoreBaeltRepository.AddTicket(ticket3);
+        StoreBaeltRepository.AddTicket(ticket4);
+
+        // Assert
+        Assert.AreEqual(2, StoreBaeltRepository.GetTicketsLicensePlate("YX12389").Count);
+    }
 }
